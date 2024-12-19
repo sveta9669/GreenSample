bcd9281006041758e900cf29eb0a189b51ffd65b
sveta9669 <80348203+sveta9669@users.noreply.github.com>
2024-12-19 21:35:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.querySelectorAll('#menu li').forEach(item => {
+//     item.addEventListener('click', function() {
+//         document.querySelectorAll('#menu li').forEach(li => {
+//             li.classList.remove('activeHeaderItem');
+//         });
+//         this.classList.add('activeHeaderItem');
+//     });
+// });
